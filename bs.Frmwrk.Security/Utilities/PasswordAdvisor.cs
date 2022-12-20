862b0652f246d2438d6c934f81adf0fd849d22d5
Fabio Cavallari <fcavallari@italcom.it>
2022-12-20 09:34:12 +0100

    Corretta logica calcolo complessita Password

--- bs.Frmwrk.Security/Utilities/PasswordAdvisor.cs
+++ bs.Frmwrk.Security/Utilities/PasswordAdvisor.cs
@@ -1,4 +1,5 @@
 ﻿using bs.Frmwrk.Core.Models.Security;
+using bs.Frmwrk.Shared;
 using System.Text.RegularExpressions;
 
 namespace bs.Frmwrk.Security.Utilities
@@ -7,7 +8,7 @@ namespace bs.Frmwrk.Security.Utilities
     {
         public static PasswordScore CheckStrength(string password)
         {
-            int score = 0;
+            int score = 1; //Very Weak
 
             if (password.Length < 1)
                 return PasswordScore.Blank;
@@ -18,15 +19,15 @@ namespace bs.Frmwrk.Security.Utilities
                 score++;
             if (password.Length >= 12)
                 score++;
-            if (Regex.Match(password, @"/\d+/", RegexOptions.ECMAScript).Success)
+            if (Regex.Match(password, @"\d+", RegexOptions.ECMAScript).Success)
                 score++;
-            if (Regex.Match(password, @"/[a-z]/", RegexOptions.ECMAScript).Success &&
-              Regex.Match(password, @"/[A-Z]/", RegexOptions.ECMAScript).Success)
+            if (Regex.Match(password, @"[a-z]", RegexOptions.ECMAScript).Success &&
+              Regex.Match(password, @"[A-Z]", RegexOptions.ECMAScript).Success)
                 score++;
-            if (Regex.Match(password, @"/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/", RegexOptions.ECMAScript).Success)
+            if (Regex.Match(password, @".[!,@,#,$,%,^,&,*,?,_,~,£,(,)]", RegexOptions.ECMAScript).Success)
                 score++;
-
-            return (PasswordScore)score;
+            
+            return score.ToEnum<PasswordScore>();
         }
     }
 }
--- bs.Frmwrk.Test/ApiTest.cs
+++ bs.Frmwrk.Test/ApiTest.cs
@@ -1,4 +1,7 @@
+using bs.Frmwrk.Core.Models.Security;
 using bs.Frmwrk.Core.Services.Auth;
+using bs.Frmwrk.Core.Services.Security;
+using bs.Frmwrk.Shared;
 using bs.Frmwrk.Test.Dtos;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,14 +9,59 @@ namespace bs.Frmwrk.Test
 {
     public class ApiTest
     {
+        /// <summary>
+        /// Resolves the authentication service and try to login.
+        /// </summary>
         [Test]
-        public async Task BootstrapApi_Test()
+        public async Task ResolveAuthService()
         {
             var authService = Root.ServiceProvider?.GetRequiredService<IAuthService>();
             Assert.That(authService, Is.Not.Null, "Cannot resolve AuthService from DI");
 
             var authResponse = await authService.AuthenticateAsync(new AuthRequestDto ("admin",  "admin" ), "test-host");
             Assert.That(authResponse, Is.Not.Null, "AuthenticateAsync doesnt work properly");
+        }
+
+        [Test]
+        public async Task ResolveSecurityService()
+        {
+            var securityService = Root.ServiceProvider?.GetRequiredService<ISecurityService>();
+            Assert.That(securityService, Is.Not.Null, "Cannot resolve SecurityService from DI");
+
+            var getPasswordScoreResponse = securityService.GetPasswordScore("pippo");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.VeryWeak), "GetPasswordScore doesnt work properly");
+
+            getPasswordScoreResponse = securityService.GetPasswordScore("giameglio");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.Weak), "GetPasswordScore doesnt work properly");
+
+            getPasswordScoreResponse = securityService.GetPasswordScore("giamegl!o");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.Medium), "GetPasswordScore doesnt work properly");
+
+            getPasswordScoreResponse = securityService.GetPasswordScore("decisamentegiameglio");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.Medium), "GetPasswordScore doesnt work properly");
+
+            getPasswordScoreResponse = securityService.GetPasswordScore("decisamentegiam3glio");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.Strong), "GetPasswordScore doesnt work properly");
+
+            getPasswordScoreResponse = securityService.GetPasswordScore("decisamentegiameglio!");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.Strong), "GetPasswordScore doesnt work properly");
+
+            getPasswordScoreResponse = securityService.GetPasswordScore("decIsamentegiameglio!");
+            Assert.That(getPasswordScoreResponse, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value, Is.Not.Null, "GetPasswordScore doesnt work properly");
+            Assert.That(getPasswordScoreResponse.Value.Id.ToEnum<PasswordScore>(), Is.EqualTo(PasswordScore.VeryStrong), "GetPasswordScore doesnt work properly");
         }
     }
 }
