6b2dd0cd29af59f1907f911b94b1560f95eb4d95
Babbubba <fcavallari@bsoftsolutions.it>
2023-05-17 11:38:11 +0200

    .

--- bs.Frmwrk.Application/Mappings/DateTimeNToDateTimeNTypeConverter.cs
+++ bs.Frmwrk.Application/Mappings/DateTimeNToDateTimeNTypeConverter.cs
@@ -1,8 +1,7 @@
 ﻿using AutoMapper;
 using bs.Frmwrk.Shared;
 
-namespace bs.Frmwrk.Application.Mappings
-{
+namespace bs.Frmwrk.Application.Mappings {
     public class DateTimeNToDateTimeNTypeConverter : ITypeConverter<DateTime?, DateTime?>
     {
         public DateTime? Convert(DateTime? source, DateTime? destination, ResolutionContext context)
