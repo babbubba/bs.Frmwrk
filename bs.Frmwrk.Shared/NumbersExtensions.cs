31f5ecbd207819557221dc89cb0f7a22f0097cb1
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2023-11-17 23:49:39 +0100

    Aggiunte estensioni per numeri e stringhe

--- bs.Frmwrk.Shared/NumbersExtensions.cs
+++ bs.Frmwrk.Shared/NumbersExtensions.cs
@@ -10,8 +10,19 @@
         /// <returns></returns>
         public static int DivideRoundingUp(int x, int y)
         {
-            int remainder;
-            int quotient = Math.DivRem(x, y, out remainder);
+            int quotient = Math.DivRem(x, y, out int remainder);
+            return remainder == 0 ? quotient : quotient + 1;
+        }
+
+        /// <summary>
+        /// Divides two long values and return an long value rounded up.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns></returns>
+        public static long DivideRoundingUp(long x, long y)
+        {
+            long quotient = Math.DivRem(x, y, out long remainder);
             return remainder == 0 ? quotient : quotient + 1;
         }
     }
